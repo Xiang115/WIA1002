8ef4fa45d3b46e2d6e66b2d21609623e18161db1
Xiang115 <gohkimxiang0115goh@gmail.com>
2024-04-24 18:07:18 +0800

    :sparkles: Add more solution

--- Lab/Lab6/Q4/Main.java
+++ Lab/Lab6/Q4/Main.java
@@ -4,9 +4,9 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        String[] words = {"dfhcdji","ckmckm","abcba","wdjiwjd","hahaha"};
-        for(String word : words){
-            System.out.printf("%s is palindrome?: %b\n",word, isPalindromeV2(word));
+        String[] words = { "dfhcdji", "ckmckm", "abcba", "wdjiwjd", "hahaha" };
+        for (String word : words) {
+            System.out.printf("%s is palindrome?: %b\n", word, isPalindromeV2(word));
         }
     }
 
@@ -16,19 +16,19 @@ public class Main {
         for (char c : words)
             stack.push(c);
         for (char c : words)
-            if (c != stack.pop()) 
+            if (c != stack.pop())
                 return false;
         return true;
     }
 
     // abcba
-    public static boolean isPalindromeV2(String word){
+    public static boolean isPalindromeV2(String word) {
         Stack<Character> stack = new Stack<>();
-        for(int i=0;i<(word.length()+1)/2;i++){
+        for (int i = 0; i < (word.length() + 1) / 2; i++) {
             stack.push(word.charAt(i));
         }
-        for(int i = word.length()/2;i<word.length();i++){
-            if(word.charAt(i) != stack.pop()){
+        for (int i = word.length() / 2; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
                 return false;
             }
         }
---
+++ Tutorial/Tutorial7/README.md
@@ -0,0 +1,9 @@
+# Tutorial 7 (Queue)  
+
+## Question 1   
+Name four everyday examples of a queue other than those discussed during lecture.
+
+## Solution (Q1)  
+```bash
+- 
+
