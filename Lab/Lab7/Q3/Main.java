e520057925ac5bcc4f55feda0fd610868b92b914
Xiang115 <gohkimxiang0115goh@gmail.com>
2024-04-25 15:15:13 +0800

    :seedling: Upload the L7Q3

---
+++ Lab/Lab7/Q3/Main.java
@@ -0,0 +1,78 @@
+package Lab.Lab7.Q3;
+
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+/*
+Buy 100 shares at $10 each
+Buy 50 shares at $20 each
+Sell 50 shares at $20 each
+Sell 60 shares at $30 each
+Sell 50 shares at $35 each
+ */
+
+public class Main {
+    private static Queue<Integer> share;
+    private static Queue<Integer> price;
+    private static int total = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        share = new LinkedList<>();
+        price = new LinkedList<>();
+
+        while (true) {
+            System.out.print("Enter your query (In format \'Buy / Sell x shares at $y each\'): ");
+            String query = sc.nextLine();
+
+            if (query.isBlank())
+                break;
+
+            String[] word = query.split(" ");
+            if (word[0].equalsIgnoreCase("Buy")) {
+                share.offer(Integer.parseInt(word[1]));
+                price.offer(Integer.parseInt(word[4].substring(1)));
+                System.out.println("Buying now...");
+                Display();
+
+            } else if (word[0].equalsIgnoreCase("Sell")) {
+                int SellShare = Integer.parseInt(word[1]);
+                int SellPrice = Integer.parseInt(word[4].substring(1));
+                System.out.println("Selling the shares now...");
+                calcTotal(SellShare, SellPrice);
+                Display();
+            }
+        }
+        sc.close();
+        System.out.println("Final Capital Gain / Loss: " + total);
+    }
+
+    public static void Display() {
+        System.out.println("Queue for Share: Queue: " + share);
+        System.out.println("Queue for Price: Queue: " + price);
+    }
+
+    public static void calcTotal(int SellShare, int SellPrice) {
+        while (SellShare > 0) {
+            if (share.isEmpty()) {
+                System.out.println("No shares to sell!");
+                break;
+            }
+
+            if (SellShare >= share.peek()) {
+                total += share.peek() * (SellPrice - price.poll());
+                SellShare -= share.poll();
+            } else {
+                total += SellShare * (SellPrice - price.peek());
+                share.offer(share.peek() - SellShare);
+                share.poll();
+                for (int i = 0; i < share.size() - 1; i++) {
+                    share.offer(share.poll());
+                }
+                SellShare = 0;
+            }
+            System.out.println("Total Capital Gain / Loss: " + total);
+        }
+    }
+}
