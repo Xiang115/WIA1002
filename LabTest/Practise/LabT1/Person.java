96754bf19f54aeaed761f4a117d58116d0e2eb8d
Xiang115 <gohkimxiang0115goh@gmail.com>
2024-05-05 15:01:50 +0800

    :sparkles: Move File

---
+++ LabTest/Practise/LabT1/Person.java
@@ -0,0 +1,19 @@
+package LabTest.LabT1;
+
+public class Person{
+    private String ContactName;
+    private String ContactNumber;
+
+    public Person(String ContactName, String ContactNumber){
+        this.ContactName = ContactName;
+        this.ContactNumber = ContactNumber;
+    }
+
+    public String getName(){
+        return this.ContactName;
+    }
+
+    public String toString(){
+        return String.format("Person{name=-%s, contactNo=-%s}", this.ContactName,this.ContactNumber);
+    }
+}
