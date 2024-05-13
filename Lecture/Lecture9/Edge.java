9b00d2e2b827ac7ba7dc38cebbb17118854cd64a
Xiang115 <gohkimxiang0115goh@gmail.com>
2024-05-13 18:59:58 +0800

    :seedling: Upload Lecture 9

--- Lecture/Lecture9/Edge.java
+++ Lecture/Lecture9/Edge.java
@@ -2,18 +2,18 @@ package Lecture.Lecture9;
 
 // WIA/WIB1002 Data Structures
 // part of Graphs implementation using List
-class Edge<T extends Comparable<T>, N extends Comparable <N>> {
-	Vertex<T,N> toVertex;
+class Edge<T extends Comparable<T>, N extends Comparable<N>> {
+	Vertex<T, N> toVertex;
 	N weight;
-	Edge<T,N> nextEdge;
-	
-	public Edge()	{
+	Edge<T, N> nextEdge;
+
+	public Edge() {
 		toVertex = null;
 		weight = null;
 		nextEdge = null;
 	}
-	
-	public Edge(Vertex<T,N> destination, N w, Edge<T,N> a)	{
+
+	public Edge(Vertex<T, N> destination, N w, Edge<T, N> a) {
 		toVertex = destination;
 		weight = w;
 		nextEdge = a;
--- Lecture/Lecture9/Vertex.java
+++ Lecture/Lecture9/Vertex.java
@@ -2,27 +2,26 @@ package Lecture.Lecture9;
 
 // WIA/WIB1002 Data Structures
 // part of Graphs implementation using List
-class Vertex<T extends Comparable<T>, N extends Comparable <N>> {
+class Vertex<T extends Comparable<T>, N extends Comparable<N>> {
    T vertexInfo;
    int indeg;
    int outdeg;
-   Vertex<T,N> nextVertex;
-   Edge<T,N> firstEdge;
-    
+   Vertex<T, N> nextVertex;
+   Edge<T, N> firstEdge;
+
    public Vertex() {
-      vertexInfo=null;
-      indeg=0;
-      outdeg=0;
+      vertexInfo = null;
+      indeg = 0;
+      outdeg = 0;
       nextVertex = null;
       firstEdge = null;
    }
-	
-   public Vertex(T vInfo, Vertex<T,N> next) {
+
+   public Vertex(T vInfo, Vertex<T, N> next) {
       vertexInfo = vInfo;
-      indeg=0;
-      outdeg=0;
+      indeg = 0;
+      outdeg = 0;
       nextVertex = next;
       firstEdge = null;
-   }	
+   }
 }
-
