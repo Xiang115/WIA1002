88174673e9292d8578110d3aaa59d9e57d527ac2
Xiang115 <gohkimxiang0115goh@gmail.com>
2024-05-15 17:55:52 +0800

    :sparkles: Modified code

--- Lab/Lab10/Q3/Main.java
+++ Lab/Lab10/Q3/Main.java
@@ -2,8 +2,8 @@ package Lab.Lab10.Q3;
 
 public class Main {
     public static long exponenet(int x, int m){
-        if(m == 1){
-            return x;
+        if(m == 0){
+            return 1;
         }
         return x * exponenet(x, m-1);
     }
